51aee727bf36dc386b0f9cc13c89a44df08eaf3a
psy-er <89902211+psy-er@users.noreply.github.com>
2024-03-24 12:25:15 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyFirstApplication"
+include(":app")
+include(":ch6_view")
+include(":ch7_layout")
