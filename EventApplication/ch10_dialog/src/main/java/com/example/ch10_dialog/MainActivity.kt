4a7d0e1265980ccc7fef6ed77192edb20664be6c
psy-er <89902211+psy-er@users.noreply.github.com>
2024-04-09 02:26:25 +0900

    ch10,ch11

--- EventApplication/ch10_dialog/src/main/java/com/example/ch10_dialog/MainActivity.kt
+++ EventApplication/ch10_dialog/src/main/java/com/example/ch10_dialog/MainActivity.kt
@@ -8,6 +8,8 @@ import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.DatePicker
 import android.widget.TimePicker
 import android.widget.Toast
@@ -16,13 +18,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ch10_dialog.databinding.ActivityMainBinding
+import com.example.ch10_dialog.databinding.DialogCustomBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // 추후 추가
         binding.btnDate.setOnClickListener {
             // month만 0부터 시작한다
@@ -88,40 +94,108 @@ class MainActivity : AppCompatActivity() {
                 //setNeutralButtom("상세정보", null)
                 show()
             }
+        }
 
-            val eventHandler = object : DialogInterface.OnClickListener {
+            var selected = 0
+            val eventHandler2 = object : DialogInterface.OnClickListener {
                 override fun onClick(dialog: DialogInterface?, which: Int) {
                     if (which == DialogInterface.BUTTON_POSITIVE) {
                         Log.d("mobileapp", "BUTTON_POSITIVE")
+                        binding.btnAlertSingle.text = "${items[selected]} 선택"
                     } else if (which == DialogInterface.BUTTON_NEGATIVE) {
                         Log.d("mobileapp", "BUTTON_NEGATIVE")
                     }
                 }
             }
-            var selected = 0
+
             binding.btnAlertSingle.setOnClickListener {
                 AlertDialog.Builder(this).run() {
-                    setTitle("알림창 - 아이템")
+                    setTitle("알림창 - Single")
                     setIcon(android.R.drawable.ic_dialog_alert)
-                    setSingleChoiceItems(items, 1, object:DialogInterface.OnClickListener{
+                    setSingleChoiceItems(items, 1, object : DialogInterface.OnClickListener {
 
                         override fun onClick(dialog: DialogInterface?, which: Int) {
                             Log.d("mobileapp", "${items[which]} 선택")
                             selected = which
                         }
                     })
-                    setPositiveButton("예", eventHandler)
-                    setNegativeButton("아니오", eventHandler)
+                    setPositiveButton("예", eventHandler2)
+                    setNegativeButton("아니오", eventHandler2)
                     //setNeutralButton("상세정보", null)
                     show()
+                }
             }
             binding.btnAlertMulti.setOnClickListener {
+                AlertDialog.Builder(this).run() {
+                    setTitle("알림창 - 다수 선택")
+                    setIcon(android.R.drawable.ic_dialog_alert)
 
+                    setMultiChoiceItems(items, booleanArrayOf(false, true, true, false), object:DialogInterface.OnMultiChoiceClickListener{
+                        override fun onClick(
+                            dialog: DialogInterface?,
+                            which: Int,
+                            isChecked: Boolean
+                        ) {
+                            Log.d("mobileapp", "${items[which]} ${if(isChecked) "선택" else "해제"}")
+                        }
+                    })
+
+                    setPositiveButton("예", eventHandler)
+                    setNegativeButton("아니오", eventHandler)
+                    show()
+                }
             }
+
+
+            val dialogBinding = DialogCustomBinding.inflate(layoutInflater)
+            val eventHandler3 = object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    if (which == DialogInterface.BUTTON_POSITIVE) {
+                        Log.d("mobileapp", "BUTTON_POSITIVE")
+                        if(dialogBinding.rbtn1.isChecked){
+                            binding.btnAlertCustom.text = dialogBinding.rbtn1.text.toString()
+                        }
+                        else if(dialogBinding.rbtn2.isChecked){
+                            binding.btnAlertCustom.text = dialogBinding.rbtn2.text.toString()
+                        }
+                    } else if (which == DialogInterface.BUTTON_NEGATIVE) {
+                        Log.d("mobileapp", "BUTTON_NEGATIVE")
+                    }
+                }
+            }
+
             binding.btnAlertCustom.setOnClickListener {
+                AlertDialog.Builder(this).run() {
+                    setTitle("알림창 - 사용자 화면")
+                    setIcon(android.R.drawable.ic_dialog_alert)
 
+                    setView(dialogBinding.root)
+
+                    setPositiveButton("예", eventHandler3)
+                    setNegativeButton("아니오", eventHandler3)
+                    show()
+                }
             }
-
         }
+    // 수업이랑 }} 개수가 다르다.. 확인해보기
+    // Option Menu
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_navigation, menu)
+        return super.onCreateOptionsMenu(menu)
     }
-}}
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.item1 -> {
+                Log.d("mobileapp", "Option Menu : 메뉴 1")
+                binding.btnDate.setTextColor(Color.parseColor("#ffff00"))
+                true
+            }
+            R.id.item2 -> true
+            R.id.item3 -> true
+            R.id.item4 -> true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
