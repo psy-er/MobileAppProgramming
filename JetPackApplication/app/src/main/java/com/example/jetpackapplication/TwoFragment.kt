9626942d7af499209598372b45023db58ae67a6e
psy-er <89902211+psy-er@users.noreply.github.com>
2024-04-15 13:44:11 +0900

    Fragment, RecyclerView

--- JetPackApplication/app/src/main/java/com/example/jetpackapplication/TwoFragment.kt
+++ JetPackApplication/app/src/main/java/com/example/jetpackapplication/TwoFragment.kt
@@ -1,15 +1,68 @@
 package com.example.jetpackapplication
 
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jetpackapplication.databinding.FragmentTwoBinding
+import com.example.jetpackapplication.databinding.ItemRecyclerviewBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+class MyViewHolder(val binding:ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
+class MyAdapter(val datas:MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        binding.itemData.text = datas[position]
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+}
+
+class MyItemDecoration(val context: Context):RecyclerView.ItemDecoration(){
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        // 그림 -> 항목
+        super.onDraw(c, parent, state)
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            0f, 0f, null)
+    }
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        // 항목 -> 그림
+        super.onDrawOver(c, parent, state)
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+            500f, 500f, null)
+    }
+
+    // 각각의 item에 대해서 꾸며준다
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        view.setBackgroundColor(Color.parseColor("#123456"))
+    }
+}
 
 /**
  * A simple [Fragment] subclass.
@@ -34,7 +87,29 @@ class TwoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        val binding = FragmentTwoBinding.inflate(inflater, container, false)
+
+        var datas = mutableListOf<String>()
+        for(i in 1..10){
+            datas.add("Item $i")
+        }
+
+        // adpter & viewHolder 작성 필수이다. MyAdapter 클래스 만든다.
+        binding.recyclerView.adapter = MyAdapter(datas)
+
+        // layoutManager로 배치하기
+        var linearlayout = LinearLayoutManager(activity)
+
+        // 가로 배치
+        linearlayout.orientation = LinearLayoutManager.HORIZONTAL
+
+        var gridlayout = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
+        binding.recyclerView.layoutManager = gridlayout //linearlayout
+
+        // 선택적
+        binding.recyclerView.addItemDecoration(MyItemDecoration(activity as Context))
+
+        return binding.root
     }
 
     companion object {
